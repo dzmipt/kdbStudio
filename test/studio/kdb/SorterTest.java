15ca0866527051623346ec5879f68d041f1c3b3e
dzmipt <dz.mipt@gmail.com>
2025-06-07 14:30:45 +0200

    introduce KColumn

--- test/studio/kdb/SorterTest.java
+++ test/studio/kdb/SorterTest.java
@@ -9,22 +9,23 @@ public class SorterTest {
     @Test
     public void testSort() {
         K.KIntVector array = new K.KIntVector(2, 1, 1, 2, 3);
-
+        KColumn column = new KColumn("test", array);
         assertArrayEquals(new int[] {1, 2, 0, 3, 4},
-                Sorter.sort(array, new int[]{0, 1, 2, 3 ,4}) );
+                Sorter.sort(column, new int[]{0, 1, 2, 3 ,4}) );
 
         assertArrayEquals(new int[] {2, 1, 0, 3, 4},
-                Sorter.sort(array, new int[]{0, 2, 1, 3 ,4}) );
+                Sorter.sort(column, new int[]{0, 2, 1, 3 ,4}) );
 
     }
 
     @Test
     public void testReverse() {
         K.KIntVector array = new K.KIntVector(2, 1, 1, 2, 3);
+        KColumn column = new KColumn("test", array);
         assertArrayEquals(new int[] {4, 0, 3, 1, 2},
-                Sorter.reverse(array, new int[]{1, 2, 0, 3, 4}) );
+                Sorter.reverse(column, new int[]{1, 2, 0, 3, 4}) );
 
         assertArrayEquals(new int[] {2, 1, 0, 3, 4},
-                Sorter.reverse(array, new int[]{4, 0, 3, 2, 1}) );
+                Sorter.reverse(column, new int[]{4, 0, 3, 2, 1}) );
     }
 }
