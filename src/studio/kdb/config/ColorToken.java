0b66ae6a72bf4197ee64c355ec33f43f8a8bbb57
dzmipt <dz.mipt@gmail.com>
2025-10-17 21:19:08 +0200

    add description

--- src/studio/kdb/config/ColorToken.java
+++ src/studio/kdb/config/ColorToken.java
@@ -3,39 +3,48 @@ package studio.kdb.config;
 import java.awt.*;
 
 public enum ColorToken {
-    DEFAULT(Color.BLACK),
-    ERROR(Color.RED),
-    WHITESPACE(Color.BLACK),
-    BRACKET(Color.BLACK),
-    OPERATOR(Color.BLACK),
-    COMMAND(new Color(240,180,0)),
-    SYSTEM(new Color(240,180,0)),
-    EOLCOMMENT( Color.GRAY),
-    KEYWORD(new Color(0,0,255)),
-    IDENTIFIER(new Color(180,160,0)),
-    SYMBOL(new Color(179,0,134)),
-    CHARVECTOR(new Color(0,200,20)),
-    BOOLEAN(new Color(51,204,255)),
-    BYTE(new Color(51,104,255)),
-    SHORT(new Color(51,104,255)),
-    LONG(new Color(51,104,255)),
-    REAL(new Color(51,104,255)),
-    INTEGER(new Color(51,104,255)),
-    FLOAT(new Color(51,104,255)),
-    TIMESTAMP(new Color(184,138,0)),
-    TIMESPAN(new Color(184,138,0)),
-    DATETIME(new Color(184,138,0)),
-    DATE(new Color(184,138,0)),
-    MONTH(new Color(184,138,0)),
-    MINUTE(new Color(184,138,0)),
-    SECOND(new Color(184,138,0)),
-    TIME(new Color(184,138,0));
+    DEFAULT("Default", Color.BLACK),
+    ERROR("Error", Color.RED),
+    WHITESPACE("Whitespace", Color.BLACK),
+    BRACKET("Brackets", Color.BLACK),
+    OPERATOR("Operators", Color.BLACK),
+    COMMAND("Commands", new Color(240,180,0)),
+    SYSTEM("System", new Color(240,180,0)),
+    EOLCOMMENT("Comments", Color.GRAY),
+    KEYWORD("Keywords", new Color(0,0,255)),
+    IDENTIFIER("Variables", new Color(180,160,0)),
+    SYMBOL("Symbols", new Color(179,0,134)),
+    CHARVECTOR("Strings ", new Color(0,200,20)),
+    BOOLEAN("Booleans", new Color(51,204,255)),
+    BYTE("Bytes", new Color(51,104,255)),
+    SHORT("Shorts", new Color(51,104,255)),
+    LONG("Longs", new Color(51,104,255)),
+    REAL("Reals", new Color(51,104,255)),
+    INTEGER("Integers", new Color(51,104,255)),
+    FLOAT("Floats", new Color(51,104,255)),
+    TIMESTAMP("Timestamps", new Color(184,138,0)),
+    TIMESPAN("Timespans", new Color(184,138,0)),
+    DATETIME("Datetimes", new Color(184,138,0)),
+    DATE("Dates", new Color(184,138,0)),
+    MONTH("Months", new Color(184,138,0)),
+    MINUTE("Minutes", new Color(184,138,0)),
+    SECOND("Seconds", new Color(184,138,0)),
+    TIME("Times", new Color(184,138,0));
 
-    private Color color;
+    private final Color color;
+    private final String description;
+
     public Color getColor() {
         return color;
     }
-    ColorToken(Color color) {
+
+    public String getDescription() {
+        return description;
+    }
+
+    ColorToken(String description, Color color) {
+        this.description = description;
         this.color = color;
     }
+
 }
