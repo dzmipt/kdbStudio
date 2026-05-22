d28a3e4c2b489972e971415cb9778b67b31766bc
dzmipt <dz.mipt@gmail.com>
2026-05-22 09:12:54 +0200

    line can span across all stacks

--- src/studio/ui/GroupLayoutSimple.java
+++ src/studio/ui/GroupLayoutSimple.java
@@ -1,5 +1,7 @@
 package studio.ui;
 
+import studio.core.Studio;
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -59,35 +61,50 @@ public class GroupLayoutSimple extends GroupLayout {
             }
         }
 
-        SequentialGroup horizontalGroup = createSequentialGroup();
+        for (int i=0; i<lineCount; i++) {
+            boolean parallel = false;
+            for (Stack stack: stacks) {
+                parallel |= stack.lines.get(i) == null;
+            }
+            for (Stack stack: stacks) {
+                Line line = stack.lines.get(i);
+                if (line != null) line.parallel = parallel;
+            }
+        }
+
+
+        SequentialGroup normalHorizontalGroup = createSequentialGroup();
         for (Stack stack: stacks) {
             ParallelGroup stackGroup = createParallelGroup(Alignment.LEADING);
             for (Line line: stack.lines) {
-                SequentialGroup lineGroup = createSequentialGroup();
-                for (int i=0; i<line.components.length; i++) {
-                    Component component = line.components[i];
-                    if (stack.maxWidth || maxWidthComponents.contains(component)) {
-                        // Why we don't add here with max=Integer.MAX_VALUE ??
-                        lineGroup.addComponent(component);
-                    } else {
-                        lineGroup.addComponent(component, PREFERRED_SIZE, PREFERRED_SIZE, PREFERRED_SIZE);
-                    }
-                    if (padding >-1 && i<line.components.length-1) lineGroup.addGap(padding);
-                }
-                if (line.glue != null) {
-                    lineGroup.addComponent(line.glue);
-                }
+                if (line == null || line.parallel) continue;
+                SequentialGroup lineGroup = line.addToLineGroup(this, createSequentialGroup(), stack.maxWidth);
                 stackGroup.addGroup(lineGroup);
             }
-            horizontalGroup.addGroup(stackGroup);
+            normalHorizontalGroup.addGroup(stackGroup);
+        }
+
+        ParallelGroup horizontalGroup = createParallelGroup().addGroup(normalHorizontalGroup);
+        for (int i=0; i<lineCount; i++) {
+            SequentialGroup lineGroup = createSequentialGroup();
+            for (Stack stack: stacks) {
+                Line line = stack.lines.get(i);
+                if (line == null) continue;
+                if (! line.parallel) continue;
+
+                line.addToLineGroup(this, lineGroup, stack.maxWidth);
+            }
+            horizontalGroup.addGroup(lineGroup);
         }
         setHorizontalGroup(horizontalGroup);
+
 
         SequentialGroup verticalGroup = createSequentialGroup();
         for (int lineIndex = 0; lineIndex<lineCount; lineIndex++) {
             ParallelGroup lineGroup = createParallelGroup(baseline ? Alignment.BASELINE : Alignment.CENTER);
             for (Stack stack: stacks) {
                 Line line = stack.lines.get(lineIndex);
+                if (line == null) continue;
                 for (int i=0; i<line.components.length; i++) {
                     Component component = line.components[i];
                         lineGroup.addComponent(component, PREFERRED_SIZE, PREFERRED_SIZE, PREFERRED_SIZE);
@@ -120,17 +137,84 @@ public class GroupLayoutSimple extends GroupLayout {
             lines.add(new Line(line, true));
             return this;
         }
+        public Stack skipLine() {
+            lines.add(null);
+            return this;
+        }
         public int size() {
             return lines.size();
         }
     }
 
     private static class Line {
+        boolean parallel = false;
         Component[] components;
         Component glue = null;
         Line(Component[] components, boolean hasGlue) {
             this.components = components;
             if (hasGlue) glue = Box.createGlue();
         }
+
+        SequentialGroup addToLineGroup(GroupLayoutSimple groupLayout, SequentialGroup lineGroup, boolean isMaxWidth) {
+            for (int i=0; i<components.length; i++) {
+                Component component = components[i];
+                if (isMaxWidth || groupLayout.maxWidthComponents.contains(component)) {
+                    // Why we don't add here with max=Integer.MAX_VALUE ??
+                    lineGroup.addComponent(component);
+                } else {
+                    lineGroup.addComponent(component, PREFERRED_SIZE, PREFERRED_SIZE, PREFERRED_SIZE);
+                }
+                if (groupLayout.padding >-1 && i<components.length-1) lineGroup.addGap(groupLayout.padding);
+            }
+            if (glue != null) {
+                lineGroup.addComponent(glue);
+            }
+            return lineGroup;
+        }
     }
+
+    private static JPanel testPanel(String name, Color color) {
+        JPanel p = new JPanel();
+        p.setBackground(color);
+        p.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        p.add(new JLabel(name));
+        return p;
+    }
+    public static void main(String... args) {
+        Studio.initLF();
+
+        JPanel A = testPanel("A", Color.RED);
+        JPanel B = testPanel("B", Color.BLUE);
+        JPanel C = testPanel("C", Color.GREEN);
+        JPanel D = testPanel("D", Color.YELLOW);
+        JPanel E = testPanel("E", Color.CYAN);
+        JPanel F = testPanel("F", Color.ORANGE);
+        JPanel G = testPanel("G", Color.PINK);
+
+        JPanel root = new JPanel();
+        GroupLayoutSimple layout = new GroupLayoutSimple(root, A, B, C, D, E, F, G);
+        layout.setStacks(
+                new GroupLayoutSimple.Stack()
+                        .addLine(A)
+                        .addLine(B)
+                        .addLine(C) ,
+                new GroupLayoutSimple.Stack()
+                        .addLine(D)
+                        .skipLine()
+                        .addLine(E) ,
+//                        .skipLine() ,
+        new GroupLayoutSimple.Stack()
+                        .addLine(F)
+                        .skipLine()
+                        .addLine(G)
+        );
+
+        JFrame frame = new JFrame("Test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(root);
+        frame.setSize(700, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
 }
